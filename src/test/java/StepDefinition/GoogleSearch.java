cab043a6acae7304b9beb13d5cb0dcdddb50b24d
Kranthi Kumar Chary Chennoji <kranthikumarchennoji@ibm.com>
2026-02-11 09:45:23 -0700

    Intial Commit of the cucumber project

---
+++ src/test/java/StepDefinition/GoogleSearch.java
@@ -0,0 +1,52 @@
+package StepDefinition;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.en.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleSearch {
+
+    WebDriver driver = null;
+
+    @Given("browser is open")
+    public void browser_is_open() 
+    {
+    	WebDriverManager.chromedriver().clearDriverCache().setup();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+        driver.manage().window().maximize();
+    }
+
+    @And("user is on google search page")
+    public void user_is_on_google_search_page() {
+        driver.navigate().to("https://google.com");
+    }
+
+    @When("user enters a text in search box")
+    public void user_enters_a_text_in_search_box() {
+        // 'q' is the name attribute for Google's search box
+        driver.findElement(By.name("q")).sendKeys("Selenium Automation");
+    }
+
+    @And("hits enter")
+    public void hits_enter() {
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+    }
+
+    @Then("user is navigated to search results")
+    public void user_is_navigated_to_search_results() {
+        if(driver.getPageSource().contains("Selenium Automation")) {
+            System.out.println("Test Passed: Results found.");
+        }
+        
+        // Always close the browser at the end
+        driver.close();
+        driver.quit();
+    }
+}
---
+++ src/test/java/StepDefinition/LoginClass.java
@@ -0,0 +1,30 @@
+package StepDefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginClass
+{
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+	    System.out.println("User is on the login page");
+	}
+
+	@When("user enters username and password")
+	public void user_enters_username_and_password() {
+	   System.out.println("User enters Username and Password");
+	}
+
+	@And("clicks on login button")
+	public void clicks_on_login_button() {
+	    System.out.println("User clicks on the Login Page");
+	}
+
+	@Then("user is navigated to the home page")
+	public void user_is_navigated_to_the_home_page() {
+	    System.out.println("User is Navigated to the Home Page");
+	}
+
+}

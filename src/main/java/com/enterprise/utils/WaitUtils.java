74e05677e041867e3f44fb309d7bea0e46fc5ed6
Kranthi Kumar Chary Chennoji <kranthikumarchennoji@ibm.com>
2026-02-16 00:21:57 -0700

    removed unused selenium packages

--- src/main/java/com/enterprise/utils/WaitUtils.java
+++ src/main/java/com/enterprise/utils/WaitUtils.java
@@ -3,7 +3,6 @@ package com.enterprise.utils;
 import java.time.Duration;
 import java.util.List;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
